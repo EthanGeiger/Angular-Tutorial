0a8cd9e3632d666399d6ca29fa56f28ae347a02a
EthanGeiger <egeiger14@gmail.com>
2022-02-01 16:15:04 -0500

    Imported HTTP client, added shipping service template

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Product } from './products';
 
 @Injectable({
@@ -8,7 +9,7 @@ export class CartService {
 
   items: Product[] = [];
   
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   addToCart(product: Product) {
     this.items.push(product);
@@ -16,6 +17,10 @@ export class CartService {
 
   getItems() {
     return this.items;
+  }
+
+  getShippingPrices() {
+    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
   }
 
   clearCart() {
